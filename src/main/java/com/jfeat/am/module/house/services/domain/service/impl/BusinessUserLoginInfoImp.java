717f624e5062d7149abbf331010f71c39e4c9a63
ye126 <jiajiajia6000@126.com>
2022-06-30 17:56:31 +0800

    供应商登录标识

--- src/main/java/com/jfeat/am/module/house/services/domain/service/impl/BusinessUserLoginInfoImp.java
+++ src/main/java/com/jfeat/am/module/house/services/domain/service/impl/BusinessUserLoginInfoImp.java
@@ -1,6 +1,7 @@
 package com.jfeat.am.module.house.services.domain.service.impl;
 
 import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.jfeat.am.core.jwt.JWTKit;
 import com.jfeat.am.module.house.services.domain.dao.QueryEndpointUserDao;
 import com.jfeat.am.module.house.services.domain.dao.QueryHousePropertyCommunityDao;
@@ -8,6 +9,8 @@ import com.jfeat.am.module.house.services.domain.dao.QueryHouseUserCommunityStat
 import com.jfeat.am.module.house.services.gen.persistence.model.EndpointUser;
 import com.jfeat.am.module.house.services.gen.persistence.model.HousePropertyCommunity;
 import com.jfeat.am.module.house.services.gen.persistence.model.HouseUserCommunityStatus;
+import com.jfeat.am.module.supplier.services.gen.persistence.dao.SupplierMapper;
+import com.jfeat.am.module.supplier.services.gen.persistence.model.Supplier;
 import com.jfeat.users.account.services.domain.service.BusinessUserLoginInfo;
 import com.jfeat.users.account.services.domain.service.impl.ServiceStore;
 import com.jfeat.users.account.services.gen.persistence.model.UserAccount;
@@ -28,16 +31,27 @@ public class BusinessUserLoginInfoImp implements BusinessUserLoginInfo {
     @Resource
     QueryHousePropertyCommunityDao queryHousePropertyCommunityDao;
 
+    @Resource
+    SupplierMapper supplierMapper;
 
     @Override
     public JSONObject expandUserInfo(UserAccount userAccount) {
         JSONObject jsonObject = new JSONObject();
-
+        Boolean isSupplier = false;
         Long communityId = null;
         String communityName = null;
         String tenantName = null;
 
         HousePropertyCommunity community = queryHousePropertyCommunityDao.queryHouseCommunityByOrgId(userAccount.getId());
+
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("end_user_id",userAccount.getId());
+        queryWrapper.last("limit 0,1");
+        Supplier supplier = supplierMapper.selectOne(queryWrapper);
+        if(supplier != null){
+            isSupplier = true;
+        }
+
         if (community!=null){
             communityId = community.getId();
             communityName = community.getCommunity();
@@ -46,6 +60,7 @@ public class BusinessUserLoginInfoImp implements BusinessUserLoginInfo {
         jsonObject.put("communityId",communityId);
         jsonObject.put("communityName",communityName);
         jsonObject.put("tenantName",tenantName);
+        jsonObject.put("isSupplier",isSupplier);
         return jsonObject;
     }
 }
