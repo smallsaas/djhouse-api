ff3ed35df7bc9ac551a67acde34164e4c5faba06
ye126 <jiajiajia6000@126.com>
2022-06-30 15:07:15 +0800

    供应商模块移除

--- src/main/java/com/jfeat/am/module/house/services/domain/model/SupplierBindType.java
+++
@@ -1,8 +0,0 @@
-package com.jfeat.am.module.house.services.domain.model;
-
-public class SupplierBindType {
-    //0-未绑定 1-绑定申请中 2-已绑定'
-    public static final Integer UNBIND = 0;
-    public static final Integer BINDING = 1;
-    public static final Integer BIND = 2;
-}
