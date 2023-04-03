8aba58dcec43e67cc6955dff0d2eb65668a24669
hhhhhtao <1799231213@qq.com>
2023-04-03 23:18:26 +0800

    修改供应商订单等

--- src/main/java/com/jfeat/am/module/house/api/app/supplier/SupplierOrderEndpoint.java
+++ src/main/java/com/jfeat/am/module/house/api/app/supplier/SupplierOrderEndpoint.java
@@ -12,6 +12,8 @@ import com.jfeat.crud.base.exception.BusinessCode;
 import com.jfeat.crud.base.exception.BusinessException;
 import com.jfeat.crud.base.tips.SuccessTip;
 import com.jfeat.crud.base.tips.Tip;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,15 +22,16 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 /**
- * @description: 供应商api，该controller主要调用vms模块（供应商项目）的service
+ * @description: 供应商订单api,此controller主要是关联 商城(saas-mall-api) 和 供应商(vms) 两个模块，涉及到供应商订单操作的api都会在此controller
  * @project: djhouse-api
  * @version: 1.0
  * @date: 2023/3/22 19:48
  * @author: hhhhhtao
  */
+@Api(value = "供应商订单")
 @RestController
 @RequestMapping("/api/u/house/supplier")
-public class VenderEndpoint {
+public class SupplierOrderEndpoint {
 
     @Resource
     VenderOverModelService venderService;
@@ -39,20 +42,23 @@ public class VenderEndpoint {
     @Resource
     UserAccountUtility userAccountUtility;
 
+    @ApiOperation(httpMethod = "GET",value="获取供应商订单列表",notes = "拥有供应商身份的用户请求才能获取到对应的订单列表，只能获取到自己（供应商）的订单列表")
     @GetMapping("/orders")
     public Tip listVenderOrders(@RequestParam(name = "pageNum",required = false,defaultValue = "1") int pageNum,
                                @RequestParam(name = "pageSize",required = false,defaultValue = "10") int pageSize) {
 
+        // 判断用户类型是否拥有供应商
         if (!userAccountUtility.judgementJurisdiction(EndUserTypeSetting.USER_TYPE_SUPPLIER)) {
             throw new BusinessException(BusinessCode.NoPermission,"没有供应商权限");
         }
 
+        // 因为遗留的问题，即使拥有了供应商类型，也有可能没有加入到供应商表，所以这里再做一个判断
         Long userId = JWTKit.getUserId();
-
         Vender vender = venderService.getIdByUserId(userId);
         if (vender == null) throw new BusinessException(BusinessCode.NoPermission,"未加入到供应商");
 
         Page<TOrder> page = new Page<>(pageNum,pageSize);
         return SuccessTip.create(orderService.listOrdersBySupplier(page,vender.getId()));
     }
+
 }
